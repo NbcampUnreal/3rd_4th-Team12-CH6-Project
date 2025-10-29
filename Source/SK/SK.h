4fdbfba982b2ac447aff6a83b11d48dfb35173bb
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-10-29 16:24:18 +0900

    feat(Macro) :GetSet 매크로 추가

--- Source/SK/SK.h
+++ Source/SK/SK.h
@@ -4,3 +4,11 @@
 
 #include "CoreMinimal.h"
 
+
+#define UE_GET_SET_INIT(Type, Name, DefaultValue, ...) \
+private: \
+UPROPERTY(__VA_ARGS__) Type Name = DefaultValue; \
+public: \
+FORCEINLINE Type Get##Name() const { return Name; } \
+FORCEINLINE void Set##Name(const Type& val) { Name = val; }
+
