0261f81672d47a7cf9402612976d13c06b345f0e
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-10-29 20:44:06 +0900

    fix: 헤더 순서 수정

--- Source/SK/GameAbilitySystem/Ability/SKGameplayAbility.cpp
+++ Source/SK/GameAbilitySystem/Ability/SKGameplayAbility.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "AbilitySystemComponent.h"
-#include "Character/SKCharacterBase.h"
+
 #include "GameAbilitySystem/Ability/SKGameplayAbility.h"
 
 USKGameplayAbility::USKGameplayAbility()
--- Source/SK/GameAbilitySystem/Attribute/SKAttributeSet.cpp
+++ Source/SK/GameAbilitySystem/Attribute/SKAttributeSet.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "GameAbilitySystem/Attribute/SKAttributeSet.h"
 #include "Net/UnrealNetwork.h"
 
-#include "GameAbilitySystem/Attribute/SKAttributeSet.h"
 
 USKAttributeSet::USKAttributeSet()
 {
