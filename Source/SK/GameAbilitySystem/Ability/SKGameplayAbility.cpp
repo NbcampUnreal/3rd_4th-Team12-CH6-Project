cb809503b01387a15c080303728ada72fa79f2f7
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-03 14:22:41 +0900

    fix(GA) :GameAbility base 수정

--- Source/SK/GameAbilitySystem/Ability/SKGameplayAbility.cpp
+++ Source/SK/GameAbilitySystem/Ability/SKGameplayAbility.cpp
@@ -8,4 +8,17 @@ USKGameplayAbility::USKGameplayAbility()
 {
 }
 
+void USKGameplayAbility::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
+
+void USKGameplayAbility::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
 
--- Source/SK/GameAbilitySystem/Ability/SKGameplayAbility.h
+++ Source/SK/GameAbilitySystem/Ability/SKGameplayAbility.h
@@ -17,6 +17,20 @@ class SK_API USKGameplayAbility : public UGameplayAbility
 public:
 	USKGameplayAbility();
 
-protected:	
+	virtual void ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData) override;
 
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+
+
+	
 };
