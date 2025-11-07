aa563f478a1b48ad56d3a515d1f9548c674b1060
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-07 22:53:09 +0900

    fix(Tag) : 신규 태그 추가

--- Source/SK/Utility/SKNativeGameplayTags.cpp
+++ Source/SK/Utility/SKNativeGameplayTags.cpp
@@ -8,6 +8,9 @@ UE_DEFINE_GAMEPLAY_TAG(TAG_Team_Monster, "Team.Monster");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Team_Monster_Boss, "Team.Monster.Boss");
 
 
+UE_DEFINE_GAMEPLAY_TAG(TAG_Object, "Object");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Object_SpikeTrap, "Object.SpikeTrap");
+
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability, "Ability");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Dash, "Ability.Dash");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Dash_Active, "Ability.Dash.Active");
