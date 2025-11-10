f0511ae0b9d1101510c1b08704841f11755da9df
gardencho59 <gardencho59@gmail.com>
2025-11-10 17:51:29 +0900

    feat(Interaction) : 상호작용 인터페이스 생성

---
+++ Source/SK/Interaction/Interface/IInteractable.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "IInteractable.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class SK_API IInteractable
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction")
+	void Interact(AActor* Interactor);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction")
+	void GetInteractionData(FInteractionData& OutData);
+};
