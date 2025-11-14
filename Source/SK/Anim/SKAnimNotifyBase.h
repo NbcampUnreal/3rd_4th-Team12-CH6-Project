364289f3a7f393580797e893576e54d10455f50b
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-14 19:45:30 +0900

    feat(AnimNotify);  SkAnimNotifyBase 추가

---
+++ Source/SK/Anim/SKAnimNotifyBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "SKAnimNotifyBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SK_API USKAnimNotifyBase : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
