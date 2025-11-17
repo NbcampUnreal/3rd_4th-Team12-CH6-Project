037109e1a310161bf18c6145db01be99cb8f85f1
seocom <wjddnjs6520@naver.com>
2025-11-17 14:24:40 +0900

    feat: 인벤토리, 장비/퀵슬롯, 장비 교체, 퀵슬롯 교체 레이아웃 생성

---
+++ Source/SK/UI/LayoutWidget/EquipmentMainLayoutWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/SKLayoutWidgetBase.h"
+#include "EquipmentMainLayoutWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SK_API UEquipmentMainLayoutWidget : public USKLayoutWidgetBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SK/UI/LayoutWidget/InventoryLayoutWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/SKLayoutWidgetBase.h"
+#include "InventoryLayoutWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SK_API UInventoryLayoutWidget : public USKLayoutWidgetBase
+{
+	GENERATED_BODY()
+	
+};
