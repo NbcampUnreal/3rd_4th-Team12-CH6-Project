38c6a335a419277783b64d9bb14b4dbb15c85976
gardencho59 <gardencho59@gmail.com>
2025-11-17 14:35:09 +0900

    fix(Character) : Tick에서 어빌리티 실행

--- Source/SK/Character/SKPlayerCharacter.cpp
+++ Source/SK/Character/SKPlayerCharacter.cpp
@@ -59,6 +59,13 @@ void ASKPlayerCharacter::BeginPlay()
 			}
 		}
 	}
+	
+	UAbilitySystemComponent* ASC = GetAbilitySystemComponent();
+	if (ASC)
+	{
+		FGameplayTag InteractTag = FGameplayTag::RequestGameplayTag(TEXT("Ability.Interact"));
+		ASC->TryActivateAbilitiesByTag(FGameplayTagContainer(InteractTag));
+	}
 }
 
 void ASKPlayerCharacter::SetSprinting(bool bSprinting)
