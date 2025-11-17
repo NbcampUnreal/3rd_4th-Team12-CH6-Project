fcb9b9ffdbe7131500e7efe0ff86365a97a9c418
LAPTOP-22BRUPA6\longr <whitegrub173@gmail.com>
2025-11-17 18:00:58 +0900

    [fix] player, ai 분리 [feat] ai gas스탯연동, hp감소테스트가능

--- Source/SK/Character/SKAICharacter.h
+++ Source/SK/Character/SKAICharacter.h
@@ -1,36 +1,18 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Character/SKCharacterBase.h"
+#include "Character/SKAICharacterBase.h"
 #include "SKAICharacter.generated.h"
 
-class UWidgetComponent;
-class UMonsterHealthWidget;
-class UMonsterDamageWidget;
-/**
- * 
- */
+
 UCLASS()
-class SK_API ASKAICharacter : public ASKCharacterBase
+class SK_API ASKAICharacter : public ASKAICharacterBase
 {
 	GENERATED_BODY()
+
 public:
 	ASKAICharacter();
 
 protected:
 	virtual void BeginPlay() override;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="UI")
-	UWidgetComponent* HPBarWidgetComponent;
-
-	UPROPERTY()
-	UMonsterHealthWidget* HPBarWidget;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="UI")
-	UWidgetComponent* DamageTextWidgetComponent;
-
-	UPROPERTY()
-	UMonsterDamageWidget* DamageTextWidget;
 };
