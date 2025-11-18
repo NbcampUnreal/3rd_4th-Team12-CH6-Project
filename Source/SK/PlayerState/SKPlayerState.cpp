d4e3e8b9d0378b8efa09912383494ee492141c5e
seocom <wjddnjs6520@naver.com>
2025-11-18 16:42:39 +0900

    feat: 장비 인스턴스 클래스, 플레이어 스테이트에 컴포넌트 추가

--- Source/SK/PlayerState/SKPlayerState.cpp
+++ Source/SK/PlayerState/SKPlayerState.cpp
@@ -3,6 +3,31 @@
 
 #include "PlayerState/SKPlayerState.h"
 
+#include "Component/EquipmentComponent.h"
+#include "Component/InventoryComponent.h"
+#include "Component/QuickSlotComponent.h"
+
+ASKPlayerState::ASKPlayerState()
+{
+	InventoryComponent = CreateDefaultSubobject<UInventoryComponent>(TEXT("InventoryComponent"));
+	if (InventoryComponent)
+	{
+		InventoryComponent->SetIsReplicated(true);
+	}
+ 
+	QuickSlotComponent = CreateDefaultSubobject<UQuickSlotComponent>(TEXT("QuickSlotComponent"));
+	if (QuickSlotComponent)
+	{
+		QuickSlotComponent->SetIsReplicated(true);
+	}
+ 
+	EquipmentComponent = CreateDefaultSubobject<UEquipmentComponent>(TEXT("EquipmentComponent"));
+	if (EquipmentComponent)
+	{
+		EquipmentComponent->SetIsReplicated(true);
+	}
+}
+
 void ASKPlayerState::CopyProperties(APlayerState* NewPlayerState)
 {
 	Super::CopyProperties(NewPlayerState);
--- Source/SK/PlayerState/SKPlayerState.h
+++ Source/SK/PlayerState/SKPlayerState.h
@@ -6,6 +6,9 @@
 #include "GameFramework/PlayerState.h"
 #include "SKPlayerState.generated.h"
 
+class UInventoryComponent;
+class UQuickSlotComponent;
+class UEquipmentComponent;
 /**
  * 
  */
@@ -15,9 +18,20 @@ class SK_API ASKPlayerState : public APlayerState
 	GENERATED_BODY()
 
 public:
+	ASKPlayerState();
 	//SeamlessTravel로 인해 PlayerState가 재성성되기 때문에 이전PS에서 데이터를 가져오기 위해 사용
 	virtual void CopyProperties(APlayerState* NewPlayerState) override;
 
 	// 네트워크 복제에 필요한 함수 재정의
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory", Replicated)
+	UInventoryComponent* InventoryComponent;
+ 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "QuickSlot", Replicated)
+	UQuickSlotComponent* QuickSlotComponent;
+ 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Equipment", Replicated)
+	UEquipmentComponent* EquipmentComponent;
+	
 };
