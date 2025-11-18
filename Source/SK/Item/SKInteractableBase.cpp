74a9ff68bd980878a85ea81350275257266dcc83
gardencho59 <gardencho59@gmail.com>
2025-11-18 16:03:01 +0900

    feat(Interaction): 상호작용 부모 클래스

---
+++ Source/SK/Item/SKInteractableBase.cpp
@@ -0,0 +1,55 @@
+#include "SKInteractableBase.h"
+
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
+ASKInteractableBase::ASKInteractableBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+	SetRootComponent(Root);
+	
+	TraceCollision = CreateDefaultSubobject<USphereComponent>("TraceCollision");
+	TraceCollision->SetupAttachment(Root);
+
+	TraceCollision->SetSphereRadius(100.0f);
+	TraceCollision->SetHiddenInGame(false);
+	// 전용 트레이스 채널 추가 필요
+	TraceCollision->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	
+	InteractionWidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("InteractionWidgetComponent"));
+	InteractionWidgetComponent->SetupAttachment(RootComponent);
+	InteractionWidgetComponent->SetVisibility(false);
+}
+
+void ASKInteractableBase::AddToInventory(AActor* Interactor)
+{
+	// 플레이어 스테이트 가져오기 및 캐스팅
+	// 인벤토리 컴포넌트 가져오기 및 캐스팅
+
+	// 인벤토리 전달용 데이터 구성
+	
+	// 인벤토리 내부 추가하는 함수 호춯
+}
+
+void ASKInteractableBase::OnShowWidget()
+{
+	// 위젯 표시
+}
+
+void ASKInteractableBase::OnHideWidget()
+{
+	// 위젯 숨김
+}
+
+UWidgetComponent* ASKInteractableBase::GetInteractionWidgetComponent() const
+{
+	return InteractionWidgetComponent;
+}
+
+
+void ASKInteractableBase::GetInteractionData_Implementation(FSKInteractionData& OutData)
+{
+	OutData = InteractionData;
+}
---
+++ Source/SK/Item/SKInteractableBase.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interaction/Interface/SKInteractable.h"
+#include "SKInteractableBase.generated.h"
+
+class USphereComponent;
+class UWidgetComponent;
+
+UCLASS()
+class SK_API ASKInteractableBase : public AActor, public ISKInteractable
+{
+	GENERATED_BODY()
+
+public:
+	ASKInteractableBase();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<USceneComponent> Root;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<USphereComponent> TraceCollision;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SK|Data")
+	FSKInteractionData InteractionData;
+	
+	virtual void GetInteractionData_Implementation(FSKInteractionData& OutData) override;
+	
+#pragma region Inventory
+protected:
+	UFUNCTION(BlueprintCallable, Category = "SK|Inventory")
+	void AddToInventory(AActor* Interactor);
+
+	// FInventorySlot InventorySlot;
+
+#pragma endregion
+
+#pragma region UI
+public:
+	UFUNCTION(BlueprintCallable, Category = "SK|UI")
+	void OnShowWidget();
+
+	UFUNCTION(BlueprintCallable, Category = "SK|UI")
+	void OnHideWidget();
+
+	// 테스트용 위젯
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SK|Data")
+	UWidgetComponent* InteractionWidgetComponent;
+	
+public:
+	UWidgetComponent* GetInteractionWidgetComponent() const;
+		
+#pragma endregion 
+};
