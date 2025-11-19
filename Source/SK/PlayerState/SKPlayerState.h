d7d86456daa49e35dd5c73b5c174e5b06c91b497
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-19 15:09:29 +0900

    fix(PlayerState) : 플레이어스테이트  + ASC

--- Source/SK/PlayerState/SKPlayerState.h
+++ Source/SK/PlayerState/SKPlayerState.h
@@ -4,7 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerState.h"
+#include "Character/SKPlayerDataAsset.h"
 #include "SKPlayerState.generated.h"
+
+class UAbilitySystemComponent;
+class USKAttributeSet;
 
 /**
  * 
@@ -15,9 +19,42 @@ class SK_API ASKPlayerState : public APlayerState
 	GENERATED_BODY()
 
 public:
+
+	ASKPlayerState();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
 	//SeamlessTravel로 인해 PlayerState가 재성성되기 때문에 이전PS에서 데이터를 가져오기 위해 사용
 	virtual void CopyProperties(APlayerState* NewPlayerState) override;
 
 	// 네트워크 복제에 필요한 함수 재정의
 	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+#pragma region GAS
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const;
+	USKAttributeSet* GetAttributeSet() const;
+	//DA ->AttributeSet 받아오는 함수
+	void SetDAPlayerStat();
+#pragma endregion
+	
+protected:
+#pragma region GAS
+
+
+	UPROPERTY(Replicated, EditDefaultsOnly, BlueprintReadOnly, Category = "SK|GAS")
+	TSoftObjectPtr<USKPlayerDataAsset> CharacterData;
+	// GAS 핵심 컴포넌트들
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SK|GAS")
+	class UAbilitySystemComponent* AbilitySystemComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SK|GAS")
+	class USKAttributeSet* AttributeSet;
+	
+#pragma endregion
+
+private:
+
 };
+
+
+
