e0c0c9d85f089b454ac29e1548bb52a02fe599ba
gardencho59 <gardencho59@gmail.com>
2025-11-24 16:20:58 +0900

    feat(PlayerABP): 공격 관련 기능 추가

--- Source/SK/Animation/SKPlayerAnimInstance.cpp
+++ Source/SK/Animation/SKPlayerAnimInstance.cpp
@@ -12,3 +12,37 @@ void USKPlayerAnimInstance::NativeInitializeAnimation()
 		OwnerCharacterMovementComponent = OwnerCharacter->GetCharacterMovement();
 	}
 }
+
+void USKPlayerAnimInstance::PlayLeftAttackAnim()
+{
+	if (LeftAttackMontages.IsValidIndex(LeftComboIndex))
+	{
+		//Montage_Play(LeftAttackMontages[LeftComboIndex]);
+	}
+}
+
+
+
+void USKPlayerAnimInstance::SetComboIndex(int32 ComboIndex)
+{
+	LeftComboIndex = ComboIndex;
+}
+
+void USKPlayerAnimInstance::SetIsAttacking(bool isAttacking)
+{
+	bLeftAttacking = isAttacking;
+}
+
+TArray<UAnimMontage*> USKPlayerAnimInstance::GetLeftATKMontage()
+{
+	return LeftAttackMontages;
+}
+
+UAnimMontage* USKPlayerAnimInstance::GetLeftATKMontageIndex()
+{
+	if (LeftAttackMontages.IsValidIndex(LeftComboIndex))
+	{
+		return LeftAttackMontages[LeftComboIndex];
+	}
+	return nullptr;
+}
--- Source/SK/Animation/SKPlayerAnimInstance.h
+++ Source/SK/Animation/SKPlayerAnimInstance.h
@@ -11,4 +11,31 @@ class SK_API USKPlayerAnimInstance : public USKBaseAnimInstance
 
 public:
 	virtual void NativeInitializeAnimation() override;
+
+	void PlayLeftAttackAnim();
+	
+	void SetComboIndex(int32 ComboIndex);
+	void SetIsAttacking(bool isAttacking);
+
+	TArray<UAnimMontage*> GetLeftATKMontage();
+	UAnimMontage* GetLeftATKMontageIndex();
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AnimMontage")
+	TArray<UAnimMontage*> LeftAttackMontages;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AnimMontage")
+	TArray<UAnimMontage*> RightAttackMontages;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AnimMontage")
+	TArray<UAnimMontage*> HitMontages;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AnimMontage")
+	TArray<UAnimMontage*> DashMontages;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	uint8 bLeftAttacking : 1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	uint8 LeftComboIndex = 0;
 };
