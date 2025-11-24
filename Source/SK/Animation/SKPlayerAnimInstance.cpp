891bc79eb8a2dd4e3ac9abf49a20155e9a489132
gardencho59 <gardencho59@gmail.com>
2025-11-24 14:56:37 +0900

    feat(PlayerABP): 플레이어 애님인스턴스

---
+++ Source/SK/Animation/SKPlayerAnimInstance.cpp
@@ -0,0 +1,14 @@
+﻿#include "SKPlayerAnimInstance.h"
+
+#include "Character/SKPlayerCharacter.h"
+
+void USKPlayerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	OwnerCharacter = Cast<ASKPlayerCharacter>(GetOwningActor());
+	if (IsValid(OwnerCharacter) == true)
+	{
+		OwnerCharacterMovementComponent = OwnerCharacter->GetCharacterMovement();
+	}
+}
---
+++ Source/SK/Animation/SKPlayerAnimInstance.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SKBaseAnimInstance.h"
+#include "SKPlayerAnimInstance.generated.h"
+
+UCLASS()
+class SK_API USKPlayerAnimInstance : public USKBaseAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+};
