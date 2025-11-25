18e02f1fc440415f3a3feadad9e0a2bfca9230ba
gardencho59 <gardencho59@gmail.com>
2025-11-25 16:38:45 +0900

    fix(Tag): 상호작용 태그 이름 수정

--- Source/SK/Utility/SKNativeGameplayTags.cpp
+++ Source/SK/Utility/SKNativeGameplayTags.cpp
@@ -18,7 +18,7 @@ UE_DEFINE_GAMEPLAY_TAG(TAG_Ability, "Ability");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Dash, "Ability.Dash");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Dash_Active, "Ability.Dash.Active");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Sprint, "Ability.Sprint");
-UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Interact, "Ability.Interact");
+UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_Interaction, "Ability.InteractionTrace");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_OpenChest, "Ability.OpenChest");
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_LeftATK, "Ability.LeftATK");
