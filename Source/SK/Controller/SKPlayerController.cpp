8783bad58c17b2eb9ed5da6dd4f7d3bf87e2db71
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-25 15:19:38 +0900

    fix(PC) ; Tick 즉시 수정

--- Source/SK/Controller/SKPlayerController.cpp
+++ Source/SK/Controller/SKPlayerController.cpp
@@ -47,20 +47,6 @@ void ASKPlayerController::Tick(float DeltaTime)
 	}
 }
 
-void ASKPlayerController::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-	if (bIsLockedOn && CurrentTarget)
-	{
-		float Dist = FVector::Dist(GetPawn()->GetActorLocation(), CurrentTarget->GetActorLocation());
-		if (Dist > LockOnRadius * 1.2f)
-		{
-			SetLockOnTarget(nullptr);
-		}
-	}
-}
-
 void ASKPlayerController::EnterDungeon()
 {
 	if (!HasAuthority())
