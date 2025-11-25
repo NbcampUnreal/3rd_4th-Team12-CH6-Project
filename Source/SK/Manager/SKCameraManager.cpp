6b3c127de8c05aee1656a08c3b3796c837b21a37
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-25 15:09:26 +0900

    feat(CM) : CameraMamaner 추가

---
+++ Source/SK/Manager/SKCameraManager.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Manager/SKCameraManager.h"
+
+ASKCameraManager::ASKCameraManager()
+{
+}
+
+void ASKCameraManager::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime)
+{
+	Super::UpdateViewTarget(OutVT, DeltaTime);
+
+	if (!bIsLockedOn || !LockedTarget)
+		return;
+
+	AActor* Player = OutVT.Target;
+
+	FVector CamLoc = GetCameraLocation(); //카메라위치
+	
+	FVector TargetLoc = LockedTarget->GetActorLocation(); //타겟위치
+	TargetLoc.Z += LockOnHeight;  // 몬스터 높이 보정
+
+
+	// 타겟 가려짐 체크
+	if (IsTargetObstructed(CamLoc, TargetLoc))
+	{
+		bIsLockedOn = false;
+		LockedTarget = nullptr;
+		return;
+	}
+
+	// 거리 초과 체크
+	float Dist = FVector::Dist(Player->GetActorLocation(), LockedTarget->GetActorLocation());
+	if (Dist > MaxLockDistance)
+	{
+		bIsLockedOn = false;
+		LockedTarget = nullptr;
+		return;
+	}
+
+	// 타겟 바라보기
+	FRotator TargetRot = (TargetLoc - CamLoc).Rotation();
+	TargetRot.Pitch -= LockOnPitch; // 숫자 조절: 5~20 정도가 자연스러움
+	FRotator NewRot = FMath::RInterpTo(GetCameraRotation(), TargetRot, DeltaTime, LockOnInterpSpeed);
+
+	GetOwningPlayerController()->SetControlRotation(NewRot);
+}
+
+bool ASKCameraManager::IsTargetObstructed(const FVector& CamLoc, const FVector& TargetLoc)
+{
+	FHitResult Hit;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(LockedTarget);
+	Params.AddIgnoredActor(GetOwningPlayerController()->GetPawn());
+
+	bool bHit = GetWorld()->LineTraceSingleByChannel(
+		Hit,
+		CamLoc,
+		TargetLoc,
+		ECC_Visibility,
+		Params
+	);
+
+	if (!bHit) return false;
+
+	return Hit.GetActor() != LockedTarget;
+}
