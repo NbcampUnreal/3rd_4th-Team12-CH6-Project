a761e2231318cac3f38e43616b4243b9a9d3de90
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-26 18:03:53 +0900

    fix(CM) ; 카메라매니저 주석추가

--- Source/SK/Manager/SKCameraManager.h
+++ Source/SK/Manager/SKCameraManager.h
@@ -26,15 +26,19 @@ public:
 	UPROPERTY(EditAnywhere,Category = "SK|LockOn")
 	float LockOnInterpSpeed = 7.f;
 
-	UPROPERTY(EditAnywhere,Category = "SK|LockOn")
+	//카메라높이
+	UPROPERTY(EditAnywhere,Category = "SK|LockOn") 
 	float LockOnHeight = 60.f;
 
+	//카메라각도
 	UPROPERTY(EditAnywhere,Category = "SK|LockOn")
 	float LockOnPitch = 10.f;
 
+	//추적최대거리
 	UPROPERTY(EditAnywhere,Category = "SK|LockOn")
 	float MaxLockDistance = 2000.f;
 
+	//추적최소거리
 	UPROPERTY(EditAnywhere,Category = "SK|LockOn")
 	float MinLockDistance = 200.f;
 	
