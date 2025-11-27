321673789365c67506f6b0568b0cf3accd359158
seocom <wjddnjs6520@naver.com>
2025-11-27 16:47:56 +0900

    fix: 리팩토링을 위한 클래스 생성

---
+++ Source/SK/UI/SlotWidget/Equipment/EquipmentItemEquipWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/SlotWidget/Equipment/EquipmentItemBaseWidget.h"
+#include "EquipmentItemEquipWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SK_API UEquipmentItemEquipWidget : public UEquipmentItemBaseWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SK/UI/SlotWidget/Equipment/EquipmentItemQuickWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/SlotWidget/Equipment/EquipmentItemBaseWidget.h"
+#include "EquipmentItemQuickWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SK_API UEquipmentItemQuickWidget : public UEquipmentItemBaseWidget
+{
+	GENERATED_BODY()
+	
+};
