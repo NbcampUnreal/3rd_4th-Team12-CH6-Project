9df7cd49e69aa6e666fdb2de457bac09020d1779
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-27 19:48:12 +0900

    fix(GA): CombatComponent랑 연동

--- Source/SK/GameAbilitySystem/Ability/SK_GA_LeftAttack_Axe.cpp
+++ Source/SK/GameAbilitySystem/Ability/SK_GA_LeftAttack_Axe.cpp
@@ -5,12 +5,13 @@
 
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
-#include "Anim/SkAnimInstance_Axe.h"
+#include "Animation/SKPlayerAnimInstance.h"
 #include "Character/SKPlayerCharacter.h"
 #include "Component/SKCombatComponent.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "GameFramework/Character.h"
 #include "Utility/SKNativeGameplayTags.h"
+#include "Weapon/SKWeaponData.h"
 
 USK_GA_LeftAttack_Axe::USK_GA_LeftAttack_Axe()
 {
@@ -32,6 +33,7 @@ void USK_GA_LeftAttack_Axe::ActivateAbility(const FGameplayAbilitySpecHandle Han
 
 	ASKPlayerCharacter* PlayerCharacter = Cast<ASKPlayerCharacter>(Character);
 	USKCombatComponent* CombatComponent = PlayerCharacter->GetCombatComponent();
+	UAbilitySystemComponent* ASC = PlayerCharacter->GetAbilitySystemComponent();
 	PlayerCharacter->UpdateMovementTag_ATK(TAG_State_Action_ATK_LeftMelee, true);
 
 	if (!CommitAbility(Handle, ActorInfo, ActivationInfo))
@@ -44,47 +46,48 @@ void USK_GA_LeftAttack_Axe::ActivateAbility(const FGameplayAbilitySpecHandle Han
 	if (!IsValid(BaseAnim))
 		return;
 
-	USkAnimInstance_Axe* AxeAnimInstance = Cast<USkAnimInstance_Axe>(BaseAnim);
-	if (!AxeAnimInstance)
+	USKPlayerAnimInstance* PlayerAnimInstance = Cast<USKPlayerAnimInstance>(BaseAnim);
+	if (!IsValid(PlayerAnimInstance))
 		return;
 
-	UAbilitySystemComponent* ASC = PlayerCharacter->GetAbilitySystemComponent();
 
-	TArray<UAnimMontage*> Array_Montage = AxeAnimInstance->GetLeftATKMontage();
+	const FWeaponDataRow* DA_Weapon = CombatComponent->GetWeaponData();
+
+	TObjectPtr<USKWeaponData> Weapon_Data = DA_Weapon->WeaponData;
+
+	TArray<TObjectPtr<UAnimMontage>> LeftATKMontage = Weapon_Data->LeftAttackMontages;
 	
+
+	TArray<UAnimMontage*> Array_Montage = PlayerAnimInstance->GetLeftATKMontage();
+
 	CombatComponent->Server_IncreaseComboIndex(true);
-	
+
 	int32 ComboIndex = CombatComponent->GetComboIndex();
 	int32 MaxIndex = LeftAttackDamageGE.Num() - 1;
 	int32 SafeIndex = FMath::Clamp(ComboIndex - 1, 0, MaxIndex);
 	UE_LOG(LogTemp, Error,
-		   TEXT("[GA] ComboIndex = %d | SafeIndex = %d | MaxIndex = %d | MontageCount = %d"),
-		   ComboIndex,
-		   SafeIndex,
-		   MaxIndex,
-		   Array_Montage.Num()
-	);
-	UAnimMontage* Montage = Array_Montage[SafeIndex];
-
-	ASC->PlayMontage(
-		this,
-		ActivationInfo,
-		Montage,
-		1.0f
+	       TEXT("[GA] ComboIndex = %d | SafeIndex = %d | MaxIndex = %d | MontageCount = %d"),
+	       ComboIndex,
+	       SafeIndex,
+	       MaxIndex,
+	       Array_Montage.Num()
 	);
 	
+	UAnimMontage* Montage = Weapon_Data->LeftAttackMontages[0];
+	FName SectionName = FName(*FString::Printf(TEXT("Combo_%02d"), SafeIndex + 1));
+	ASC->PlayMontage(this, ActivationInfo, Montage, 1.0f);
+	PlayerAnimInstance->Montage_JumpToSection(SectionName, Montage);
+	
+
 	// 클라이언트 전용
 	if (ActorInfo->IsLocallyControlled())
 	{
 		UAnimInstance* AIM = Character->GetMesh()->GetAnimInstance();
-		if (AIM)
-		{
-			AIM->Montage_Play(Montage, 1.f);
-		}
+		AIM->Montage_Play(Montage, 1.f);
+		AIM->Montage_JumpToSection(SectionName, Montage);
 	}
-
-
-	CombatComponent->Client_PlayMontage(Montage);
+	
+	CombatComponent->Client_PlayMontage(Montage, SectionName);
 }
 
 void USK_GA_LeftAttack_Axe::EndAbility(const FGameplayAbilitySpecHandle Handle,
@@ -114,7 +117,7 @@ bool USK_GA_LeftAttack_Axe::CheckCost(const FGameplayAbilitySpecHandle Handle,
 		ACharacter* Character = Cast<ACharacter>(ActorInfo->AvatarActor.Get());
 		if (!IsValid(Character))
 			return result;
-	
+
 		ASKPlayerCharacter* PlayerCharacter = Cast<ASKPlayerCharacter>(Character);
 		USKCombatComponent* CombatComponent = PlayerCharacter->GetCombatComponent();
 		CombatComponent->ResetComboState();
@@ -134,19 +137,19 @@ void USK_GA_LeftAttack_Axe::ApplyDamageFromTrace()
 	int LeftATKIndex = CombatComponent->GetComboIndex();
 	int32 MaxIndex = LeftAttackDamageGE.Num() - 1;
 	int32 SafeIndex = FMath::Clamp(LeftATKIndex, 0, MaxIndex);
-	
+
 	for (AActor* HitActor : CombatComponent->GetHitActors())
 	{
 		if (!HitActor)
 			continue;
-	
+
 		TSubclassOf<UGameplayEffect> EffectClass = LeftAttackDamageGE[SafeIndex];
-	
+
 		FGameplayEffectSpecHandle SpecHandle = MakeOutgoingGameplayEffectSpec(EffectClass, 1.f);
-	
+
 		// Target의 AbilitySystemComponent 가져오기
 		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitActor);
-	
+
 		if (SpecHandle.IsValid() && TargetASC)
 		{
 			TargetASC->ApplyGameplayEffectSpecToSelf(*SpecHandle.Data.Get());
