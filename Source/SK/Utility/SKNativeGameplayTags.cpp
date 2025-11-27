7aedb0ea3b0d21d2beef9794b29c603aeecc9a5a
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-11-27 19:37:29 +0900

    fix(TAG) : Weapon Tag 추가

--- Source/SK/Utility/SKNativeGameplayTags.cpp
+++ Source/SK/Utility/SKNativeGameplayTags.cpp
@@ -24,7 +24,7 @@ UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_OpenChest, "Ability.OpenChest");
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_LeftATK, "Ability.LeftATK");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_LeftATK_Axe, "Ability.LeftATK.Axe");
-
+UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_LeftATK_TwoHanded, "Ability.LeftATK.TwoHanded");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_LeftATK_Axe_Num01, "Ability.LeftATK.Axe.Num01");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_LeftATK_Axe_Num02, "Ability.LeftATK.Axe.Num02");
 UE_DEFINE_GAMEPLAY_TAG(TAG_Ability_LeftATK_Axe_Num03, "Ability.LeftATK.Axe.Num03");
