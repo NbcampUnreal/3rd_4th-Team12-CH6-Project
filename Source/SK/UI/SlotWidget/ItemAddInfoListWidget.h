721c57163a560035c0f66c8ea5c16f8a9da0a47b
seocom <wjddnjs6520@naver.com>
2025-11-28 15:52:07 +0900

    fix: 아이템 추가 팝업 UI List 위젯 추가

---
+++ Source/SK/UI/SlotWidget/ItemAddInfoListWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ItemAddInfoListWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SK_API UItemAddInfoListWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
