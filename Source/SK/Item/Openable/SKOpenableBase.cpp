2a1e746086b49d563f3432dcd056dc389f3cf81b
gardencho59 <gardencho59@gmail.com>
2025-12-01 14:12:31 +0900

    feat(Interactable): 상자,문 베이스 클래스 생성

---
+++ Source/SK/Item/Openable/SKOpenableBase.cpp
@@ -0,0 +1,33 @@
+﻿#include "SKOpenableBase.h"
+#include "Components/SphereComponent.h"
+
+ASKOpenableBase::ASKOpenableBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+		
+	OverlapCollision = CreateDefaultSubobject<USphereComponent>("OverlapCollision");
+	OverlapCollision->SetupAttachment(Root);	
+	OverlapCollision->SetSphereRadius(500.0f);
+	OverlapCollision->SetRelativeLocation(FVector(0.0f, 0.0f, 500.0f));
+	OverlapCollision->SetCollisionProfileName(TEXT("Interact"));
+
+	InteractionCollision->SetSphereRadius(50.0f);
+	InteractionCollision->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
+	
+	InteractionPoint = CreateDefaultSubobject<USceneComponent>("InteractionPoint");
+	InteractionPoint->SetupAttachment(Root);
+}
+
+void ASKOpenableBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	InteractionData.InteractionLocation = InteractionPoint->GetComponentLocation();
+	InteractionData.InteractionRotation = InteractionPoint->GetComponentRotation();
+}
+
+void ASKOpenableBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
