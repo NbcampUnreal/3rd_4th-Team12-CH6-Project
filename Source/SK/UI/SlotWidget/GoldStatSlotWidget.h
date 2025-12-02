14d8a7f8c80acaa22c2af58895a7985ee12fb173
seocom <wjddnjs6520@naver.com>
2025-12-02 13:47:33 +0900

    feat: 골드 관련 UI 추가

---
+++ Source/SK/UI/SlotWidget/GoldStatSlotWidget.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "GoldStatSlotWidget.generated.h"
+
+struct FGameplayEffectSpec;
+class UTextBlock;
+class USKAttributeSet;
+/**
+ * 
+ */
+UCLASS()
+class SK_API UGoldStatSlotWidget : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+	
+protected:
+	UPROPERTY()
+	const USKAttributeSet* AttributeSet;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* TotalGoldText;
+
+	UPROPERTY(meta = (BindWidget))
+	UTextBlock* PlusGoldText;
+	
+	UPROPERTY(Transient, meta=(BindWidgetAnim))
+	UWidgetAnimation* GoldAddAnimation;
+
+	mutable float PendingGold = 0.0f;
+	mutable bool bIsPlayingAnimation = false;
+	
+	void GlodChanged(AActor* EffectInstigator, AActor* EffectCauser, const FGameplayEffectSpec* EffectSpec, float EffectMagnitude, float OldValue, float NewValue);
+
+	UFUNCTION()
+	void OnGoldAddAnimationFinished();
+};
