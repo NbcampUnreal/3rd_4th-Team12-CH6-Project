298e792e08f37df96c5718cd5487a419f19f4b89
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-12-03 15:50:47 +0900

    fix(AN) : HasAuthority추가로 클라에서 재생되도 중복으로 처리못하게 처리

--- Source/SK/Anim/SKAnimNotify_StartAttackTrace.cpp
+++ Source/SK/Anim/SKAnimNotify_StartAttackTrace.cpp
@@ -18,7 +18,7 @@ void USKAnimNotify_StartAttackTrace::Notify(USkeletalMeshComponent* MeshComp, UA
 	}
 
 	AActor* OwnerActor = MeshComp->GetOwner();
-	if (!IsValid(OwnerActor))
+	if (!IsValid(OwnerActor)|| !OwnerActor->HasAuthority())
 	{
 		//UE_LOG(LogTemp, Warning, TEXT("AnimNotify failed to get Owner Actor."));
 		return; 
