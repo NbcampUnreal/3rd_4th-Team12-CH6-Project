9e7ca4be825c23caa289a1334828a07c6a7a750a
seocom <wjddnjs6520@naver.com>
2025-12-03 14:21:40 +0900

    feat: 던전 이동 UI 기능 구현

--- Source/SK/UI/SlotWidget/DungeonSelectSlotWidget.h
+++ Source/SK/UI/SlotWidget/DungeonSelectSlotWidget.h
@@ -6,6 +6,8 @@
 #include "Blueprint/UserWidget.h"
 #include "DungeonSelectSlotWidget.generated.h"
 
+class USizeBox;
+class UButton;
 /**
  * 
  */
@@ -13,5 +15,49 @@ UCLASS()
 class SK_API UDungeonSelectSlotWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+protected:
+	virtual void NativeConstruct() override;
+ 
+	// Buttons for selecting dungeons and closing the widget
+	UPROPERTY(meta = (BindWidget))
+	UButton* Dungeon1Button;
+ 
+	UPROPERTY(meta = (BindWidget))
+	UButton* Dungeon2Button;
+ 
+	UPROPERTY(meta = (BindWidget))
+	UButton* CloseButton;
+ 
+	// SizeBoxes to hold Image/Text groups for dungeon info visibility toggling
+	UPROPERTY(meta = (BindWidget))
+	USizeBox* Dungeon1InfoBox;
+ 
+	UPROPERTY(meta = (BindWidget))
+	USizeBox* Dungeon2InfoBox;
+ 
+	// Button hover event handlers
+	UFUNCTION()
+	void OnDungeon1Hovered();
+ 
+	UFUNCTION()
+	void OnDungeon1Unhovered();
+ 
+	UFUNCTION()
+	void OnDungeon2Hovered();
+ 
+	UFUNCTION()
+	void OnDungeon2Unhovered();
+ 
+	// Button click event handlers
+	UFUNCTION()
+	void OnDungeon1Clicked();
+ 
+	UFUNCTION()
+	void OnDungeon2Clicked();
+ 
+	UFUNCTION()
+	void OnCloseClicked();
+ 
+private:
+	void SetDungeonInfoVisibility(USizeBox* InfoBox, bool bVisible);	
 };
