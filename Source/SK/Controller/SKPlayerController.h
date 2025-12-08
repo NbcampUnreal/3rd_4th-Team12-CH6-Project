d3bcdb699ab45f40615025a7f5197c70793244a2
gardencho59 <gardencho59@gmail.com>
2025-12-08 12:42:09 +0900

    fix(Controller): MoveDirection 8방향으로 수정

--- Source/SK/Controller/SKPlayerController.h
+++ Source/SK/Controller/SKPlayerController.h
@@ -12,7 +12,11 @@ enum class EMoveDirection : uint8
 {
 	None        UMETA(DisplayName = "None"),
 	Forward     UMETA(DisplayName = "Forward"),
+	ForwardLeft     UMETA(DisplayName = "ForwardLeft"),
+	ForwardRight     UMETA(DisplayName = "ForwardRight"),
 	Backward    UMETA(DisplayName = "Backward"),
+	BackwardLeft    UMETA(DisplayName = "BackwardLeft"),
+	BackwardRight    UMETA(DisplayName = "BackwardRight"),
 	Left        UMETA(DisplayName = "Left"),
 	Right       UMETA(DisplayName = "Right")
 };
