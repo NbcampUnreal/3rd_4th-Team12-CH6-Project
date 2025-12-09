c1ed5f474b8d701a3841e2235b2b36be4439caa9
gardencho59 <gardencho59@gmail.com>
2025-12-09 17:27:18 +0900

    feat(Fireplace): 화톳불 클래스 생성

---
+++ Source/SK/Item/Openable/Fireplace/SKBonfire.cpp
@@ -0,0 +1,36 @@
+﻿#include "SKBonfire.h"
+
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+
+
+ASKBonfire::ASKBonfire()
+{
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	MeshComponent->SetupAttachment(Root);
+
+	NiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>("NiagaraComponent");
+	NiagaraComponent->SetupAttachment(MeshComponent);
+	NiagaraComponent->SetRelativeScale3D(FVector(2.0f));
+	NiagaraComponent->SetRelativeLocation(FVector(0.f, 0.f, 60.0f));
+	
+	InteractionCollision->SetSphereRadius(300.0f);
+	InteractionCollision->SetRelativeLocation(FVector(0.0f, 0.0f, 70.0f));
+	
+	ObjectType = EObjectType::Fireplace;
+}
+
+void ASKBonfire::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASKBonfire::ExecuteInteraction_Implementation(AActor* Interactor)
+{
+	// ResetAttribute(); -> 구현 위치?
+	// ResetMonster(); -> 구현 위치?
+	
+	// HUD 제거  -> 구현 위치?
+	// 화톳불 전용 UI 표시 -> 구현 위치?
+}
---
+++ Source/SK/Item/Openable/Fireplace/SKBonfire.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/Openable/SKOpenableBase.h"
+#include "SKBonfire.generated.h"
+
+class UNiagaraComponent;
+
+UCLASS()
+class SK_API ASKBonfire : public ASKOpenableBase
+{
+	GENERATED_BODY()
+
+public:
+	ASKBonfire();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components|StaticMesh")
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Components|Niagara")
+	TObjectPtr<UNiagaraComponent> NiagaraComponent;
+
+#pragma region Interaction
+	
+public:
+	virtual void ExecuteInteraction_Implementation(AActor* Interactor) override;
+
+protected:
+	
+#pragma endregion
+};
