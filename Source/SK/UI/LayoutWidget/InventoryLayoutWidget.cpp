1f16a600a84d9072b471a9d0fe26c18b88cedcfb
seocom <wjddnjs6520@naver.com>
2025-12-10 15:27:09 +0900

    feat: 인벤토리 위젯 키 바인딩

--- Source/SK/UI/LayoutWidget/InventoryLayoutWidget.cpp
+++ Source/SK/UI/LayoutWidget/InventoryLayoutWidget.cpp
@@ -13,7 +13,12 @@ void UInventoryLayoutWidget::NativeConstruct()
 
 	InventoryToInGameHandle = RegisterUIActionBinding(FBindUIActionArgs(InventoryToInGameData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleInventoryToInGameAction)));
 	InventoryToEscapeHandle = RegisterUIActionBinding(FBindUIActionArgs(InventoryToEscapeData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleInventoryEscapeAction)));
-	
+	MoveUpHandle = RegisterUIActionBinding(FBindUIActionArgs(MoveUpData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleMoveUpAction)));
+	MoveDownHandle = RegisterUIActionBinding(FBindUIActionArgs(MoveDownData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleMoveDownAction)));
+	MoveLeftHandle = RegisterUIActionBinding(FBindUIActionArgs(MoveLeftData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleMoveLeftAction)));
+	MoveRightHandle = RegisterUIActionBinding(FBindUIActionArgs(MoveRightData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleMoveRightAction)));
+	SelectionHandle = RegisterUIActionBinding(FBindUIActionArgs(SelectionData, true, FSimpleDelegate::CreateUObject(this, &ThisClass::HandleSelectionAction)));
+
 	if (CloseButton)
 	{
 		CloseButton->OnClicked.Clear(); // 혹시 중복 방지
@@ -51,6 +56,81 @@ void UInventoryLayoutWidget::HandleInventoryEscapeAction()
 			MessageSubsystem->BroadcastMessage(TAG_Message_Channel_SwitchLayout, Message);
 
 			UE_LOG(LogTemp, Log, TEXT("Broadcast SwitchLayout Message: %s"), *Message.LayoutTag.ToString());
+		}
+	}
+}
+
+void UInventoryLayoutWidget::HandleMoveUpAction()
+{
+	if (UWorld* World = GetWorld())
+	{
+		if (USKGameplayMessageSubsystem* MessageSubsystem = USKGameplayMessageSubsystem::Get(World))
+		{
+			// 전송할 메시지 생성
+			FUIInteractionMoveMessage Message(EUIMessageType::Inventory, 0);
+
+			// 메시지 브로드캐스트 (UI 전환용 채널로)
+			MessageSubsystem->BroadcastMessage(TAG_Message_Channel_UIInteraction, Message);
+		}
+	}
+}
+
+void UInventoryLayoutWidget::HandleMoveDownAction()
+{
+	if (UWorld* World = GetWorld())
+	{
+		if (USKGameplayMessageSubsystem* MessageSubsystem = USKGameplayMessageSubsystem::Get(World))
+		{
+			// 전송할 메시지 생성
+			FUIInteractionMoveMessage Message(EUIMessageType::Inventory, 1);
+
+			// 메시지 브로드캐스트 (UI 전환용 채널로)
+			MessageSubsystem->BroadcastMessage(TAG_Message_Channel_UIInteraction, Message);
+		}
+	}
+}
+
+void UInventoryLayoutWidget::HandleMoveLeftAction()
+{
+	if (UWorld* World = GetWorld())
+	{
+		if (USKGameplayMessageSubsystem* MessageSubsystem = USKGameplayMessageSubsystem::Get(World))
+		{
+			// 전송할 메시지 생성
+			FUIInteractionMoveMessage Message(EUIMessageType::Inventory, 2);
+
+			// 메시지 브로드캐스트 (UI 전환용 채널로)
+			MessageSubsystem->BroadcastMessage(TAG_Message_Channel_UIInteraction, Message);
+		}
+	}
+}
+
+void UInventoryLayoutWidget::HandleMoveRightAction()
+{
+	if (UWorld* World = GetWorld())
+	{
+		if (USKGameplayMessageSubsystem* MessageSubsystem = USKGameplayMessageSubsystem::Get(World))
+		{
+			// 전송할 메시지 생성
+			FUIInteractionMoveMessage Message(EUIMessageType::Inventory, 3);
+
+			// 메시지 브로드캐스트 (UI 전환용 채널로)
+			MessageSubsystem->BroadcastMessage(TAG_Message_Channel_UIInteraction, Message);
+		}
+	}
+}
+
+void UInventoryLayoutWidget::HandleSelectionAction()
+{
+	if (UWorld* World = GetWorld())
+	{
+		if (USKGameplayMessageSubsystem* MessageSubsystem = USKGameplayMessageSubsystem::Get(World))
+		{
+			// 전송할 메시지 생성
+			FUIInteractionMoveMessage Message(EUIMessageType::Inventory, 4);
+
+			// 메시지 브로드캐스트 (UI 전환용 채널로)
+			MessageSubsystem->BroadcastMessage(TAG_Message_Channel_UIInteraction, Message);
 		}
 	}
 }
