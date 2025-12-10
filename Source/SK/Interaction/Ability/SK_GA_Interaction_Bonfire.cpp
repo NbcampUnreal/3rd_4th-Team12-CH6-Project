47ccae5dbddd3e9e06f81a91c3622f579891f7e3
gardencho59 <gardencho59@gmail.com>
2025-12-10 15:58:46 +0900

    feat(Interaction): Bonfire 상호작용 클래스 생성

---
+++ Source/SK/Interaction/Ability/SK_GA_Interaction_Bonfire.cpp
@@ -0,0 +1,58 @@
+﻿#include "SK_GA_Interaction_Bonfire.h"
+#include "Character/SKPlayerCharacter.h"
+#include "Item/SKInteractableBase.h"
+
+void USK_GA_Interaction_Bonfire::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	ASKPlayerCharacter* PlayerCharacter = Cast<ASKPlayerCharacter>(GetAvatarActorFromActorInfo());
+
+	FVector TargetLocation = GetTargetLocation(PlayerCharacter);
+	TargetLocation.Z = PlayerCharacter->GetActorLocation().Z; 
+	
+	const float Distance = GetDistance(PlayerCharacter, TargetLocation);
+	const float Duration = Distance/300.0f;
+
+	if (Distance < DistanceThreshold)
+	{
+		FVector NewTargetLocation = CachedTargetActor->GetActorLocation();
+		NewTargetLocation.Z = PlayerCharacter->GetActorLocation().Z;
+		PlayerCharacter->SetActorRotation(GetTargetRotation(PlayerCharacter, NewTargetLocation));
+		OnMoveCompleted();
+	}
+	else
+	{
+		PlayerCharacter->SetActorRotation(GetTargetRotation(PlayerCharacter, TargetLocation));
+		MoveToLocation(PlayerCharacter, TargetLocation, Duration);
+	}
+	
+}
+
+void USK_GA_Interaction_Bonfire::OnMoveCompleted()
+{	
+	ASKPlayerCharacter* PlayerCharacter = Cast<ASKPlayerCharacter>(GetAvatarActorFromActorInfo());
+
+	SetForceMove(PlayerCharacter, false);
+	
+	PlayAnimMontage(CachedInteractionData.InteractMontage);
+	
+	ExecuteTargetInteraction(Cast<UObject>(CachedTargetActor), PlayerCharacter);
+}
+
+FVector USK_GA_Interaction_Bonfire::GetTargetLocation(const ASKPlayerCharacter* PlayerCharacter) const
+{
+	const FVector TargetLocation = CachedTargetActor->GetActorLocation();
+	const FVector CharacterLocation = PlayerCharacter->GetActorLocation();
+	
+	FVector FlatTarget = TargetLocation;
+	FVector FlatCharacter = CharacterLocation;
+	FlatTarget.Z = 0.f;
+	FlatCharacter.Z = 0.f;
+	
+	FVector Direction = (FlatCharacter - FlatTarget).GetSafeNormal();
+	
+	return TargetLocation + Direction * TargetDistance;
+}
