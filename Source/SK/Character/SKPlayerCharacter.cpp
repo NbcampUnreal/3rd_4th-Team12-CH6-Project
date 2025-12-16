aba3d3a1d04454122bb51f24a942fc9d7ccbd86e
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-12-16 14:35:45 +0900

    fix(Player) : BattleComp추가

--- Source/SK/Character/SKPlayerCharacter.cpp
+++ Source/SK/Character/SKPlayerCharacter.cpp
@@ -7,6 +7,7 @@
 #include "Controller/SKPlayerController.h"
 #include "GameAbilitySystem/Attribute/SKAttributeSet.h"
 #include "AbilitySystemGlobals.h"
+#include "Component/BattleComponent.h"
 #include "Component/SKCombatComponent.h"
 #include "GameData/WeaponDataRow.h"
 #include "GameFramework/CharacterMovementComponent.h"
@@ -50,6 +51,8 @@ ASKPlayerCharacter::ASKPlayerCharacter()
 
 	// 컴뱃컴포넌트 활성화
 	CombatComponent = CreateDefaultSubobject<USKCombatComponent>(TEXT("CombatComponent"));
+
+	BattleComponent = CreateDefaultSubobject<UBattleComponent>(TEXT("BattleComponent"));
 
 
 	// InteractionComponent
@@ -195,11 +198,13 @@ void ASKPlayerCharacter::SetTraceSocket()
 		return;
 
 	CombatComponent->InitializeWeaponSocket(Row);
-
+	BattleComponent->InitializeWeaponSocket(Row);
+	
 	const FWeaponDataRow* DataRow = PS->GetWeaponDataRow();
 	if (!DataRow)
 		return;
 	CombatComponent->InitializeWeaponData(DataRow);
+	BattleComponent->InitializeWeaponData(DataRow);
 }
 
 void ASKPlayerCharacter::SetLockOnState(bool bLock)
@@ -359,6 +364,11 @@ void ASKPlayerCharacter::SetPlayerStateTag()
 USKCombatComponent* ASKPlayerCharacter::GetCombatComponent() const
 {
 	return CombatComponent;
+}
+
+UBattleComponent* ASKPlayerCharacter::GetBattleComponent() const
+{
+	return BattleComponent;
 }
 
 void ASKPlayerCharacter::OnAnimInitialized()
