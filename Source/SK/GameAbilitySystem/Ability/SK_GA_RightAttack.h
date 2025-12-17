219f3f4b56f5020d83ee90b19f6ad20a8b96a89b
seocom <wjddnjs6520@naver.com>
2025-12-17 17:17:55 +0900

    feat: 우클리 공격 클래스 생성

---
+++ Source/SK/GameAbilitySystem/Ability/SK_GA_RightAttack.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameAbilitySystem/Ability/SKGameplayAbility.h"
+#include "SK_GA_RightAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SK_API USK_GA_RightAttack : public USKGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
