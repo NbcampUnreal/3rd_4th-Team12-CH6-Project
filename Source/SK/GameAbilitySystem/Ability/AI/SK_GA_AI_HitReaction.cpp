6405cfe52da5b9c0cbebe2805dbe5b00fe0881d8
LAPTOP-22BRUPA6\longr <whitegrub173@gmail.com>
2025-12-18 11:58:49 +0900

    [feat] AI 피격어빌리티

---
+++ Source/SK/GameAbilitySystem/Ability/AI/SK_GA_AI_HitReaction.cpp
@@ -0,0 +1,73 @@
+#include "GameAbilitySystem/Ability/AI/SK_GA_AI_HitReaction.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+
+USK_GA_AI_HitReaction::USK_GA_AI_HitReaction()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+	
+	AbilityTags.AddTag(FGameplayTag::RequestGameplayTag(TEXT("Ability.HitReaction")));
+	//ActivationRequiredTags.AddTag(FGameplayTag::RequestGameplayTag(TEXT("State.Alive")));
+	//ActivationBlockedTags.AddTag(FGameplayTag::RequestGameplayTag(TEXT("Status.Stunned")));
+	ActivationOwnedTags.AddTag(FGameplayTag::RequestGameplayTag(TEXT("AI.HitReaction")));
+}
+
+void USK_GA_AI_HitReaction::HitReaction(TObjectPtr<UAnimMontage> AnimMontage)
+{
+	//SetFocus();
+	
+	OwnMontageTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+				this,
+				NAME_None,
+				AnimMontage,
+				1.0f,
+				NAME_None,
+				false,
+				1.0f
+				);
+	OwnMontageTask->OnCompleted.AddDynamic(this, &USK_GA_AI_HitReaction::OnHitReactionCompleted);
+	//OwnMontageTask->OnInterrupted.AddDynamic(this, &USK_GA_AI_HitReaction::OnMontageInterrupted);
+	//OwnMontageTask->OnCancelled.AddDynamic(this, &USK_GA_AI_HitReaction::OnMontageCancelled);
+	//OwnMontageTask->OnBlendOut.AddDynamic(this, &USK_GA_AI_HitReaction::OnMontageBlendOut);
+	OwnMontageTask->ReadyForActivation();
+}
+
+void USK_GA_AI_HitReaction::OnHitReactionCompleted()
+{
+	EndAbility(CachedHandle, CachedActorInfo, CachedActivationInfo, true, false);
+}
+
+void USK_GA_AI_HitReaction::ActivateAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData
+	)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	CommonEventTask->EndTask();
+
+	TObjectPtr<UAnimMontage> AnimMontage = GetAnimMontage("HitReaction");
+	if (!IsValid(AnimMontage))
+	{
+		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
+		return;
+	}
+	
+	HitReaction(AnimMontage);
+}
+
+void USK_GA_AI_HitReaction::EndAbility(
+	const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	bool bReplicateEndAbility,
+	bool bWasCancelled
+	)
+{
+	//ClearFocus();
+	//태그제거
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
---
+++ Source/SK/GameAbilitySystem/Ability/AI/SK_GA_AI_HitReaction.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameAbilitySystem/Ability/AI/SK_GA_AI_Base.h"
+#include "SK_GA_AI_HitReaction.generated.h"
+
+UCLASS()
+class SK_API USK_GA_AI_HitReaction : public USK_GA_AI_Base
+{
+	GENERATED_BODY()
+
+public:
+	USK_GA_AI_HitReaction();
+
+	void HitReaction(TObjectPtr<UAnimMontage> AnimMontage);
+
+	UFUNCTION()
+	void OnHitReactionCompleted();
+	
+protected:
+	virtual void ActivateAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		const FGameplayEventData* TriggerEventData
+	) override;
+
+	virtual void EndAbility(
+		const FGameplayAbilitySpecHandle Handle,
+		const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo,
+		bool bReplicateEndAbility,
+		bool bWasCancelled
+	) override;
+};
