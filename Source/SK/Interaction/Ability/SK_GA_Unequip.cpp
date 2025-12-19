40be986be1a9cc550c40dd76ad33399d7c002238
gardencho59 <gardencho59@gmail.com>
2025-12-19 10:52:53 +0900

    feat(GameplayAbility): 장착 해제 GA 생성

---
+++ Source/SK/Interaction/Ability/SK_GA_Unequip.cpp
@@ -0,0 +1,70 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SK_GA_Unequip.h"
+
+#include "AbilitySystemComponent.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "Character/SKPlayerCharacter.h"
+#include "Weapon/ActorComponent/SKActionComponent.h"
+#include "Weapon/ActionData/SKWeaponAnimData.h"
+
+USK_GA_Unequip::USK_GA_Unequip()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+}
+
+void USK_GA_Unequip::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                        const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                        const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	ASKPlayerCharacter* Char = Cast<ASKPlayerCharacter>(GetAvatarActorFromActorInfo());
+	if (!Char)
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+		return;
+	}
+	
+	USKActionComponent* ActionComponent = Char->GetActionComponent();
+	if (!ActionComponent)
+	{
+		EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, true);
+		return;
+	}
+	
+	USKWeaponAnimData* WeaponAnimData = ActionComponent->GetWeaponAnimData();
+	if (!WeaponAnimData) return;
+	
+	UAnimMontage* UnequipMontage = WeaponAnimData->UnequipMontage;
+	if (!UnequipMontage)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("UnequipMontage is null"));
+	}
+	
+	if (UnequipMontage)
+	{
+		UAbilityTask_PlayMontageAndWait* PlayAnimTask = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this, TEXT("Unequip"), UnequipMontage);
+		PlayAnimTask->OnCompleted.AddDynamic(this, &ThisClass::OnCompleted);
+		PlayAnimTask->OnInterrupted.AddDynamic(this, &ThisClass::OnCanceled);
+		PlayAnimTask->ReadyForActivation();
+	}
+}
+
+void USK_GA_Unequip::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled)
+{
+	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
+}
+
+void USK_GA_Unequip::OnCompleted()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
+
+void USK_GA_Unequip::OnCanceled()
+{
+	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+}
