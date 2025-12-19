fadd2d62c31d9750dfa5d037338e131f3cadcc02
gardencho59 <gardencho59@gmail.com>
2025-12-19 10:53:34 +0900

    feat(AnimNotify): 카타나 회전용 애님노티파이

---
+++ Source/SK/Weapon/AnimNotify/SKAN_SwitchTransform.cpp
@@ -0,0 +1,23 @@
+﻿#include "SKAN_SwitchTransform.h"
+
+#include "Character/SKPlayerCharacter.h"
+#include "Weapon/ActorComponent/SKActionComponent.h"
+
+void USKAN_SwitchTransform::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	Super::Notify(MeshComp, Animation);
+
+	ASKPlayerCharacter* Char = Cast<ASKPlayerCharacter>(MeshComp->GetOwner());
+	if (!Char)
+	{
+		return;
+	}
+
+	USKActionComponent* ActionComponent = Char->GetActionComponent();
+	if (!ActionComponent)
+	{
+		return;
+	}
+	AActor* Weapon = ActionComponent->WeaponActors[0];
+	Weapon->SetActorRelativeRotation(FRotator(-30.f, 0, 180.f));
+}
