e30987837d3b07e201c94a5dedf42c0c7ee87dce
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-12-22 21:51:50 +0900

    fix(GA) : 스킬쪽  무브먼트 방어코드 추가

--- Source/SK/GameAbilitySystem/Ability/SK_GA_Skill_01.cpp
+++ Source/SK/GameAbilitySystem/Ability/SK_GA_Skill_01.cpp
@@ -9,6 +9,7 @@
 #include "Character/SKPlayerCharacter.h"
 #include "Component/BattleComponent.h"
 #include "GameData/StaticData/ComboTableRow.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "Utility/SKNativeGameplayTags.h"
 
 void USK_GA_Skill_01::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
@@ -37,22 +38,22 @@ void USK_GA_Skill_01::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	FName SectionName = GetComboMontageSection(CurrentComboIndex);
 
 	//몽타주 실행 추가
-	
+
 	UAnimMontage* Montage = CurrentBattleComponent->GetSkillMontage(MontageIndex);
 
 	UAbilityTask_PlayMontageAndWait* PlayTask =
-	UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
-		this,
-		NAME_None,
-		Montage,
-		1.0f,
-		SectionName
-	);
-	
+		UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(
+			this,
+			NAME_None,
+			Montage,
+			1.0f,
+			SectionName
+		);
+
 	PlayTask->OnCompleted.AddDynamic(this, &USK_GA_Skill_01::OnMontageCompleted);
 	PlayTask->OnInterrupted.AddDynamic(this, &USK_GA_Skill_01::OnMontageInterrupted);
 	PlayTask->OnCancelled.AddDynamic(this, &USK_GA_Skill_01::OnMontageInterrupted);
-	
+
 	//사용 O
 	CachedCharacter->SetLooseTag(TAG_State_Action_ATK, true);
 	PlayTask->ReadyForActivation();
@@ -61,7 +62,6 @@ void USK_GA_Skill_01::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 	{
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
 	}
-	
 }
 
 void USK_GA_Skill_01::EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
@@ -74,16 +74,24 @@ void USK_GA_Skill_01::EndAbility(const FGameplayAbilitySpecHandle Handle, const 
 
 	ASKPlayerCharacter* PlayerCharacter = Cast<ASKPlayerCharacter>(Character);
 
-	UBattleComponent* BattleComponent = PlayerCharacter->GetBattleComponent();
+	// UBattleComponent* BattleComponent = PlayerCharacter->GetBattleComponent();
 
 	PlayerCharacter->UpdateMovementTag_ATK(TAG_State_Action_ATK_Skill, false);
 	PlayerCharacter->SetLooseTag(TAG_State_Action_ATK, false);
+
+
+	//방어코드 추가
+	UCharacterMovementComponent* MoveComp = Character->GetCharacterMovement();
+	if (!MoveComp)
+		return;
+	MoveComp->SetMovementMode(MOVE_Walking);
+
 
 	Super::EndAbility(Handle, ActorInfo, ActivationInfo, bReplicateEndAbility, bWasCancelled);
 }
 
 void USK_GA_Skill_01::CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility)
+                                    const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility)
 {
 	Super::CancelAbility(Handle, ActorInfo, ActivationInfo, bReplicateCancelAbility);
 }
@@ -92,12 +100,13 @@ bool USK_GA_Skill_01::CheckCost(const FGameplayAbilitySpecHandle Handle, const F
                                 FGameplayTagContainer* OptionalRelevantTags) const
 {
 	bool result = Super::CheckCost(Handle, ActorInfo, OptionalRelevantTags);
-	if (!result)
-	{
-		CachedCharacter->UpdateMovementTag_ATK(TAG_State_Action_ATK_Skill, false);
-	}
 
-	return Super::CheckCost(Handle, ActorInfo, OptionalRelevantTags);
+	// if (!result && CachedCharacter)
+	// {
+	// 	CachedCharacter->UpdateMovementTag_ATK(TAG_State_Action_ATK_Skill, false);
+	// }
+
+	return result;
 }
 
 void USK_GA_Skill_01::BindComboCache()
@@ -105,14 +114,14 @@ void USK_GA_Skill_01::BindComboCache()
 	if (!CurrentComboTable)
 	{
 		UE_LOG(LogTemp, Warning,
-			TEXT("BindComboCache failed: ComboTable is null"));
+		       TEXT("BindComboCache failed: ComboTable is null"));
 		return;
 	}
 
 	if (CurrentComboTable->GetRowStruct() != FComboTableRow::StaticStruct())
 	{
 		UE_LOG(LogTemp, Error,
-			TEXT("BindComboCache failed: RowStruct mismatch"));
+		       TEXT("BindComboCache failed: RowStruct mismatch"));
 		return;
 	}
 
@@ -130,11 +139,10 @@ void USK_GA_Skill_01::BindComboCache()
 
 		FSkillComboKey Key;
 		Key.FromState = Row->FromState;
-		Key.InputTag  = Row->InputTag;
+		Key.InputTag = Row->InputTag;
 
 		if (ComboCache.Contains(Key))
 		{
-			
 			continue;
 		}
 
