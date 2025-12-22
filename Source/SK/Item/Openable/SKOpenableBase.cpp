4bd469b004ee56d4c0b51574f7179008e74a6325
gardencho59 <gardencho59@gmail.com>
2025-12-22 18:43:06 +0900

    fix(Openable): 인접 UI 표시 기능 추가

--- Source/SK/Item/Openable/SKOpenableBase.cpp
+++ Source/SK/Item/Openable/SKOpenableBase.cpp
@@ -1,23 +1,37 @@
 ﻿#include "SKOpenableBase.h"
 
+#include "Character/SKPlayerCharacter.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Interaction/ActorComponent/SKInteractionComponent.h"
 
 ASKOpenableBase::ASKOpenableBase()
 {
-	OverlapCollision = CreateDefaultSubobject<USphereComponent>("OverlapCollision");
-	OverlapCollision->SetupAttachment(Root);	
-	OverlapCollision->SetSphereRadius(1000.0f);
-	// OverlapCollision->SetRelativeLocation(FVector(0.0f, 0.0f, 500.0f));
-	// OverlapCollision->SetCollisionProfileName(TEXT("Interact"));
+	DetectCollision = CreateDefaultSubobject<USphereComponent>("DetectCollision");
+	DetectCollision->SetupAttachment(Root);	
+	DetectCollision->SetSphereRadius(800.0f);
+	// DetectCollision->SetRelativeLocation(FVector(0.0f, 0.0f, 500.0f));
+	// DetectCollision->SetCollisionProfileName(TEXT("Interact"));
+	DetectCollision->OnComponentBeginOverlap.AddDynamic(this, &ASKOpenableBase::OnDetectBeginOverlap);
+	DetectCollision->OnComponentEndOverlap.AddDynamic(this, &ASKOpenableBase::OnDetectEndOverlap);
+	DetectCollision->SetIsReplicated(true);
 
 	InteractionCollision->SetSphereRadius(100.0f);
 	InteractionCollision->SetRelativeLocation(FVector(0.0f, 0.0f, 100.0f));
 	
 	InteractionPoint = CreateDefaultSubobject<UCapsuleComponent>("InteractionPoint");
 	InteractionPoint->SetupAttachment(Root);
+	InteractionPoint->SetCapsuleSize(34.f, 88.f);
+	InteractionPoint->SetRelativeLocation(FVector(0.f, 0.f, 90.f));
+	InteractionPoint->SetRelativeRotation(FRotator(0.f, -90.f, 0.f));
 	
 	ObjectType = EObjectType::Openable;
+
+	DetectWidget = CreateDefaultSubobject<UWidgetComponent>("DetectWidget");
+	DetectWidget->SetupAttachment(Root);
+	DetectWidget->SetWidgetSpace(EWidgetSpace::Screen);
+	DetectWidget->SetVisibility(false);
 }
 
 void ASKOpenableBase::BeginPlay()
@@ -26,5 +40,45 @@ void ASKOpenableBase::BeginPlay()
 	
 	InteractionData.InteractionLocation = InteractionPoint->GetComponentLocation();
 	InteractionData.InteractionRotation = InteractionPoint->GetComponentRotation();
+
+	
+	if (!DetectWidgetClass) return;
+
+	if (DetectWidgetClass)
+	{
+		DetectWidget->SetWidget(DetectWidgetClass);
+	}
+}
+
+void ASKOpenableBase::OnDetectBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                           UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!bCanInteract) return;
+	
+	ASKPlayerCharacter* Char = Cast<ASKPlayerCharacter>(OtherActor);
+	if (!Char) return;
+	
+	if (!Char->IsLocallyControlled()) return;
+	
+	DetectWidget->SetVisibility(true);
+}
+
+void ASKOpenableBase::OnDetectEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (!bCanInteract) return;
+	
+	ASKPlayerCharacter* Char = Cast<ASKPlayerCharacter>(OtherActor);
+	if (!Char) return;
+
+	if (!Char->IsLocallyControlled()) return;
+	
+	DetectWidget->SetVisibility(false);
+}
+
+void ASKOpenableBase::HandleCanInteractChanged()
+{
+	InteractionWidget->SetVisibility(false);
+	DetectWidget->SetVisibility(false);
 }
 
--- Source/SK/Item/Openable/SKOpenableBase.h
+++ Source/SK/Item/Openable/SKOpenableBase.h
@@ -4,6 +4,7 @@
 #include "Item/SKInteractableBase.h"
 #include "SKOpenableBase.generated.h"
 
+class ASKPlayerCharacter;
 class UCapsuleComponent;
 
 UCLASS()
@@ -15,11 +16,32 @@ public:
 	ASKOpenableBase();
 		
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
-	TObjectPtr<USphereComponent> OverlapCollision;
+	TObjectPtr<USphereComponent> DetectCollision;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
 	TObjectPtr<UCapsuleComponent> InteractionPoint;
 
 protected:
 	virtual void BeginPlay() override;
+
+#pragma region Near UI
+	UFUNCTION()
+	void OnDetectBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, 
+						UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, 
+						bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnDetectEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+							  UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	virtual void HandleCanInteractChanged() override;
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UWidgetComponent> DetectWidget;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SK|Data|UI")
+	TObjectPtr<UUserWidget> DetectWidgetClass;
+	
+#pragma endregion
 };
