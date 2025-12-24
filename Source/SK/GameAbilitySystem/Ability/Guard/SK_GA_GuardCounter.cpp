9e75b608e99279afa38258e54eea12a399199eb5
LEEJS256 <68937552+LEEJS256@users.noreply.github.com>
2025-12-24 15:47:02 +0900

    fix(GA) : 가드카운터 데미지처리 누락된거 수정

--- Source/SK/GameAbilitySystem/Ability/Guard/SK_GA_GuardCounter.cpp
+++ Source/SK/GameAbilitySystem/Ability/Guard/SK_GA_GuardCounter.cpp
@@ -144,6 +144,11 @@ void USK_GA_GuardCounter::ApplyDamageFromTrace()
 		// Target의 AbilitySystemComponent 가져오기
 		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitActor);
 
+		if (!TargetASC)
+			continue;
+		
+		DamagedActors.Add(HitActor);
+		
 		FGameplayEffectSpecHandle SpecHandle =
 			MakeOutgoingGameplayEffectSpec(GuardCounterDamageEffect, 1.f);
 
