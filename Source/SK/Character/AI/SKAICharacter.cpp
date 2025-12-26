39d344e9897e4067e427e66389460009ea9407c9
seocom <wjddnjs6520@naver.com>
2025-12-26 16:02:51 +0900

    fix: 몬스터 HP 이미지, 캐릭터 HP 바 이미지 변경

--- Source/SK/Character/AI/SKAICharacter.cpp
+++ Source/SK/Character/AI/SKAICharacter.cpp
@@ -15,7 +15,7 @@ ASKAICharacter::ASKAICharacter()
 	
 	HealthWidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("HealthWidget"));
 	HealthWidgetComponent->SetupAttachment(RootComponent);
-	HealthWidgetComponent->SetWidgetSpace(EWidgetSpace::World); // 화면 고정형
+	HealthWidgetComponent->SetWidgetSpace(EWidgetSpace::Screen); // 화면 고정형
 	HealthWidgetComponent->SetDrawSize(FVector2D(200.f, 10.f));
 
 	DamageWidgetComponent = CreateDefaultSubobject<UWidgetComponent>(TEXT("DamageWidget"));
