aca9709f3ac71ba6c7281cf2c84e9b8700106214
seocom <wjddnjs6520@naver.com>
2025-12-31 20:42:59 +0900

    feat: 화톳불 레벨업 인포 위젯 추가

--- Source/SK/Utility/SKNativeGameplayTags.cpp
+++ Source/SK/Utility/SKNativeGameplayTags.cpp
@@ -164,6 +164,7 @@ UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Slot_BonfireMenu, "UI.Slot.BonfireMenu");
 UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Slot_Option, "UI.Slot.Option");
 UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Slot_Dialogue, "UI.Slot.Dialogue");
 UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Slot_InputInfo, "UI.Slot.InputInfo");
+UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Slot_LevelUpInfo, "UI.Slot.LevelInfo");
 
 
 UE_DEFINE_GAMEPLAY_TAG(TAG_PlayerState, "PlayerState");
