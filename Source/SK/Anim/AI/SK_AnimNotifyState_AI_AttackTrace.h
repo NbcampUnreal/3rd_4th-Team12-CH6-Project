7d8870dd2e231dac363ac96536cefc3ddc9cf7e8
LAPTOP-22BRUPA6\longr <whitegrub173@gmail.com>
2026-01-11 18:56:40 +0900

    [fix] 노티파이 스테이트의 멤버변수들 캐릭터로 이관

--- Source/SK/Anim/AI/SK_AnimNotifyState_AI_AttackTrace.h
+++ Source/SK/Anim/AI/SK_AnimNotifyState_AI_AttackTrace.h
@@ -10,7 +10,6 @@ class SK_API USK_AnimNotifyState_AI_AttackTrace : public UAnimNotifyState
 	GENERATED_BODY()
 
 protected:
-	// 이걸 몽타주 안 노티파이스테이트 디테일에서 지정.
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Trace")
 	TArray<FName> SocketNames;
 
@@ -19,17 +18,6 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Trace")
 	float CapsuleHalfHeight = 20.f;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Trace")
-	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypes;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Trace")
-	TArray<TObjectPtr<AActor>> IgnoreActors;
-	
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Trace")
-	TSet<TObjectPtr<AActor>> HitActors;
-
-	TMap<FName, FVector> PrevSocketLocations;
 
 
 public:
